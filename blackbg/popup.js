99cb8228e865a3abff238e68578d183eb5f1699d
JMartinTorres <jmartintorres@outlook.es>
2019-10-09 20:34:10 +0200

    FUNCIONAMIENTO BÁSICO DE AÑADIR/ELIMINAR. Método para cargas páginas en popup

--- blackbg/popup.js
+++ blackbg/popup.js
@@ -1,6 +1,8 @@
 // Manage interaction with the popup and the selected pages list.
 
 var selectedPages = '';
+var pageNumber = 0;
+
 $( document ).ready(function() {
 	
 	// Add current page button
@@ -16,40 +18,10 @@ $( document ).ready(function() {
 			selectedPages = result.selectedPages;
 			
 			// Display every page name with its own toggle switch
-			selectedPages.forEach(function (value, i) {
-				var div = document.createElement("div");
-				div.className = 'pageList';
+			selectedPages.forEach(function (value) {
 				
-				var label = document.createElement("label");
-				label.className = 'switch';
-				label.id = 'lbl' + i;
+				addSelectedPagePopup(value.split('/blv_ck_bg/')[0], value.split('/blv_ck_bg/')[1].localeCompare('enabled') == 0);
 				
-				var input = document.createElement("input");
-				input.type = 'checkbox';
-				input.id = 'switch' + i;
-				if (value.split('/blv_ck_bg/')[1].localeCompare('enabled') == 0)
-					input.checked = true;
-				
-				var span = document.createElement("span");
-				span.className = 'slider round';
-				
-				var pageSpan = document.createElement("span");
-				pageSpan.className = 'pageName';
-				pageSpan.innerHTML = value.split('/blv_ck_bg/')[0];
-				
-				var removeImg = document.createElement("input");
-				removeImg.type = 'image';
-				removeImg.src = "icons/delete.png"
-				removeImg.id = 'remove' + i;
-
-				label.appendChild(input);
-				label.appendChild(span);
-				
-				div.appendChild(pageSpan);
-				div.appendChild(label);
-				div.appendChild(removeImg);
-				
-				document.getElementById("saveListSelection").appendChild(div); 
 			});	
 			
 		} else {
@@ -73,11 +45,63 @@ function getCurrentDomain(tabs) {
 	var currentPage = domain + '/blv_ck_bg/enabled';
 	
 	selectedPages.push(currentPage);
-	chrome.storage.sync.set({'selectedPages': selectedPages}, function() { console.log (selectedPages); })
+	chrome.storage.sync.set({'selectedPages': selectedPages}, function() { addSelectedPagePopup(domain, true); })
   
 }
 
+function addSelectedPagePopup(domain, enabled) {
+	
+	var div = document.createElement("div");
+	div.className = 'pageList';
+	
+	var label = document.createElement("label");
+	label.className = 'switch';
+	label.id = 'lbl' + pageNumber;
+	
+	var input = document.createElement("input");
+	input.type = 'checkbox';
+	input.id = 'switch' + pageNumber;
+	if (enabled)
+		input.checked = true;
+	
+	var span = document.createElement("span");
+	span.className = 'slider round';
+	
+	var pageSpan = document.createElement("span");
+	pageSpan.className = 'pageName';
+	pageSpan.innerHTML = domain;
+	
+	var removeImg = document.createElement("input");
+	removeImg.type = 'image';
+	removeImg.src = "icons/delete.png"
+	removeImg.id = 'remove' + pageNumber;
+	//removeImg.onclick = deleteSelectedPage();
 
+	label.appendChild(input);
+	label.appendChild(span);
+	
+	div.appendChild(pageSpan);
+	div.appendChild(label);
+	div.appendChild(removeImg);
+	
+	document.getElementById("saveListSelection").appendChild(div);
+	
+	// Delete selected page from list (popup and storage; asks for confirmation)
+	$('#remove' + pageNumber).bind('click', { index: pageNumber }, deleteSelectedPage);
+	
+	pageNumber++;
+  
+}
+
+function deleteSelectedPage(event) {
+	
+	selectedPages.splice(event.data.index, 1);
+	
+	if (confirm ('Are you sure you want to remove this page from the list?')) {	
+		chrome.storage.sync.set({'selectedPages': selectedPages}, function() { location.reload(true); })
+	}
+  
+}
 
 
 
